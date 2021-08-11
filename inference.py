2ae42d7424c5b485ff36e71f229201a4def54641
ID56 <mashrurmahmud@iut-dhaka.edu>
2021-08-12 00:48:57 +0600

    Added missing tqdm import

--- inference.py
+++ inference.py
@@ -1,12 +1,13 @@
 from argparse import ArgumentParser
 from config_parser import get_config
 import torch
-
 from utils.misc import get_model
 from utils.dataset import get_loader
+from tqdm import tqdm
 import os
 import glob
 import json
+
 
 @torch.no_grad()
 def get_preds(net: nn.Module, dataloader: DataLoader, device: torch.device) -> list:
@@ -90,7 +91,7 @@ if __name__ == "__main__":
     parser.add_argument("--ckpt", type=str, required=True, help="Path to checkpoint file.")
     parser.add_argument("--inp", type=str, required=True, help="Path to input. Can be a path to a .wav file, or a path to a folder containing .wav files.")
     parser.add_argument("--out", type=str, default="./", help="Path to output folder. Predictions will be stored in {out}/preds.json.")
-    parser.add_argument("--lmap", type=str, default=None, help="Path to label_map.json. If not provided, will save predictions as class indices instead of class names.")))
+    parser.add_argument("--lmap", type=str, default=None, help="Path to label_map.json. If not provided, will save predictions as class indices instead of class names.")
     parser.add_argument("--device", type=str, default="auto", help="One of auto, cpu, or cuda.")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size for batch inference.")
     
